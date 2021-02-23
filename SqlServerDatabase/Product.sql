ddd67aee8dd4bb5bf99de094231f1d8842436373
Bipeen Sinha <bipeen.sinha@hotmail.com>
2021-02-23 22:13:04 +0530

    Update Product.sql

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -1,9 +1,3 @@
-﻿CREATE TABLE [dbo].[Product]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(50) NULL, 
-    [Cost] INT NULL, 
-    [Color] NVARCHAR(50) NULL
-)
+
 
 insert into product (id,name,cost,color) values (1,"Ravi Kumar",200,"red")
